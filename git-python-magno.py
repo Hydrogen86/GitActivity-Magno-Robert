cab03cc386db940a05196706811d5984cdce2aab
MagnoRobert <magnorobert86@gmail.com>
2024-03-06 17:20:34 +0800

    Updated

---
+++ git-python-magno.py
@@ -0,0 +1,8 @@
+
+print("Robert M. Magno")
+print("Brgy. Mahunig, Gasan, Marinduque")
+print("October 23, 1999")
+
+print("College of Information and Computing Sciences")
+print("BS Information Technology")
+print("2-C")
--- myscript.py
+++
@@ -1,1 +0,0 @@
-print('Robert Magno')
